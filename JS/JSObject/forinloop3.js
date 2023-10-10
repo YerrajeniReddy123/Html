d58c60e3100f8bb502d2e9b3a5bd16df3c208fe7
YerrajeniReddy123 <mpldilip1099@gmail.com>
2023-10-10 21:57:06 +0530

    ydr

---
+++ JS/JSObject/forinloop3.js
@@ -0,0 +1,14 @@
+let emp={
+    id:101,
+    ename:'ydr',
+    sal:544,
+}
+for (key in emp) 
+{
+    console.log(key,':',emp[key])
+
+}
+
+for(ydr in emp){
+    console.log(emp)
+}
---
+++ JS/JSObject/function.js
@@ -0,0 +1,11 @@
+let emp={
+    id:10,
+    ename:'ydr',
+    sal:14545,
+    details:function(){
+        return 'dilip'
+    }
+}
+
+console.log(emp.details)
+console.log(emp.details())
