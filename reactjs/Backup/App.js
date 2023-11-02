01c037cee237462d4f8d4b8f9a71fc6da1a1ccfc
YerrajeniReddy123 <mpldilip1099@gmail.com>
2023-11-02 22:00:33 +0530

    ydr

---
+++ reactjs/Backup/App.js
@@ -0,0 +1,6 @@
+function App(){
+    return <div>
+                <h1>App Component</h1> 
+            </div>
+}
+export default App
---
+++ reactjs/Backup/class1/App.js
@@ -0,0 +1,6 @@
+let App=()=>{
+    return <div>
+        <h1>Fat Arrow Function</h1>
+    </div>    
+}
+export default App
